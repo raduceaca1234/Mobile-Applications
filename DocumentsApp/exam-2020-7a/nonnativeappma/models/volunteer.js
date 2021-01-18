0f82975417b205b2b0cdaac1d6c20d12a288eced
raduceaca1234 <ceaca.radu@yahoo.com>
2021-01-19 01:06:55 +0200

    Documents App Exam

---
+++ DocumentsApp/exam-2020-7a/nonnativeappma/models/volunteer.js
@@ -0,0 +1,12 @@
+class Volunteer {
+  constructor(id, name, status, owner, size, usage) {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+    this.owner = owner;
+    this.size = size;
+    this.usage = usage;
+  }
+}
+
+export default Volunteer;
